d7683331b40bd5fa7178c3ab74efe714443bf6ab
Marcel Samyn <dev@mrcl.io>
2016-11-13 10:15:18 +0100

    Use Angular form directives

--- ng-semantic/input/input.ts
+++ ng-semantic/input/input.ts
@@ -1,8 +1,22 @@
 import {
-    Component, Input, ChangeDetectionStrategy, Output, ViewContainerRef,
-    EventEmitter, OnInit
+  Component, Input, ChangeDetectionStrategy, Output, ViewContainerRef,
+  EventEmitter, OnInit, forwardRef
 } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { FormControl, NG_VALUE_ACCESSOR, ControlValueAccessor } from "@angular/forms";
+
+const noop = () => {};
+
+// From @angular/material
+function coerceBooleanProperty(value: any): boolean {
+  return value != null && `${value}` !== 'false';
+}
+
+export const SM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => SemanticInputComponent),
+  multi: true
+};
+
 
 /**
  * Implementation of Input element
@@ -14,24 +28,54 @@ import { FormControl } from "@angular/forms";
  */
 @Component({
   selector: "sm-input",
-  template: `<div class="field" [ngClass]="{error: (!control.valid && control.dirty && isInsideForm) }">
+  template: `<div class="field" [ngClass]="{error: (!control.valid && (control.dirty || control.touched) && isInsideForm), 'required': required }">
   <label *ngIf="label && isInsideForm">{{label}}</label>
-  <div class="ui input {{class}}" [ngClass]="{'icon': icon, 'error': (!control.valid && control.dirty &&!isInsideForm)}">
+  <div class="ui input {{class}}" [ngClass]="{'icon': icon, 'error': (!control.valid && (control.dirty || control.touched) &&!isInsideForm)}">
   <label *ngIf="label && !isInsideForm" class="ui label">{{label}}</label>
-  <input [type]="type" [formControl]="control" (keyup)="modelChange.emit(input.value)" #input placeholder="{{placeholder}}">
+  <input
+    [type]="type"
+    [(ngModel)]="model"
+    [required]="required"
+    #input #control="ngModel"
+    [placeholder]="placeholder"
+    (change)="_handleChange($event)"
+    (blur)="_handleBlur($event)"
+    >
   <i *ngIf="icon" class="{{icon}} icon"></i>
 </div>
-</div>`
+</div>`,
+  providers: [SM_INPUT_CONTROL_VALUE_ACCESSOR]
 })
-export class SemanticInputComponent implements OnInit {
+export class SemanticInputComponent implements OnInit, ControlValueAccessor {
   @Input() label: string;
   @Input() class: string;
   @Input() icon: string;
   @Input() type: string = "text";
   @Input() placeholder: string;
-  @Input() model: {};
-  @Input() control: FormControl = new FormControl();
-  @Output() modelChange: EventEmitter<string|number> = new EventEmitter<string|number>();
+
+  private _required: boolean = false;
+
+  @Input()
+  get required(): boolean { return this._required; }
+  set required(value) {
+    this._required = coerceBooleanProperty(value);
+  }
+
+  private _model: string|number = '';
+
+  get model(): any {
+    return this._model;
+  }
+  @Input()
+  set model(m: any) {
+    if (m != this._model) {
+      this._model = m;
+      this._onChangeCallback(m);
+    }
+  }
+
+  private _onTouchedCallback: () => void = noop;
+  private _onChangeCallback: (_: any) => void = noop;
 
   private isInsideForm: boolean = false;
 
@@ -42,6 +86,28 @@ export class SemanticInputComponent implements OnInit {
     if (this.inForm(this.viewRef.element.nativeElement, "form")) {
       this.isInsideForm = true;
     }
+  }
+
+  // ControlValueAccessor implementation
+  writeValue(value: string|number) {
+    this._model = value;
+  }
+
+  registerOnChange(fn: (_: any) => void) {
+    this._onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: () => void) {
+    this._onTouchedCallback = fn;
+  }
+
+  _handleBlur(event: FocusEvent) {
+    this._onTouchedCallback();
+  }
+
+  _handleChange(event: Event) {
+    this.model = (<HTMLInputElement> event.target).value;
+    this._onTouchedCallback();
   }
 
   inForm(el: Node, classname: string): boolean {
@@ -67,7 +133,7 @@ export class SemanticInputComponent implements OnInit {
   selector: "sm-checkbox",
   template: `<div class="field" [ngClass]="{error: (!control.value && control?.validator) }">
     <div class="ui {{classType}} checkbox">
-      <input type="checkbox" 
+      <input type="checkbox"
       [attr.value]="value"
       [attr.type]="inputType" tabindex="0" [attr.name]="name" [formControl]="control" [attr.disabled]="disabled">
       <label *ngIf="label">{{label}}</label>
